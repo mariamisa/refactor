f5d2c052cd39afbfd85303c78527e097fc133c6a
Israa91 <eng.israa.1991@gmail.com>
2020-12-31 21:07:07 +0200

    make the edit functionality Relates #14

--- israa-react-todo/src/components/Todo.js
+++ israa-react-todo/src/components/Todo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 import {FaTrash} from 'react-icons/fa'
 import {AiFillEdit} from 'react-icons/ai'
-import {AiOutlineCheck} from 'react-icons/ai'
-export const Todo=({text,todo,todos,setTodos})=>{
+import {FiCheckSquare} from 'react-icons/fi'
+import {EditTodoForm} from './EditTodoForm'
+export const Todo=({text,todo,todos,setTodos,edit,setEdit,inputValue,setInputValue,updatedValue,setUpdatedValue})=>{
 const deleteHandler =()=>{
     setTodos(todos.filter((element)=>{ return element.id !== todo.id}))
 }
@@ -20,14 +22,25 @@ setTodos(todos.map((element)=>{
 }))
 }
 
+
     return (
-        <div className="todo__div">
-            
-            <li className={`todo__item ${todo.completed?"completed":""}`} >{text} 
-            <button className="complete" onClick={completeHandler}><AiOutlineCheck/></button>
-                <button className="fas fa-trash-alt" onClick={deleteHandler}><FaTrash className="trash"/></button>
-                <button className="fas fa-edit-alt"><AiFillEdit/></button>
+        <div className="todo__div" key={todo.id} edit={!edit}>
+         
+           <li className={`todo-row ${todo.completed?"todo-row completed":""}`} >{text} 
+           <div className='icons'>
+            <button className="complete-icon" onClick={completeHandler}><FiCheckSquare/></button>
+                <button className="delete-icon" onClick={deleteHandler}><FaTrash className="trash"/></button>
+                <button className="edit-icon" onClick={()=>setEdit(todo.id)}><AiFillEdit/></button>
+                </div>
                 </li>
+                
+                
+                   
+                  
+                 
+                
+        
+            
         </div>
     )
 }
