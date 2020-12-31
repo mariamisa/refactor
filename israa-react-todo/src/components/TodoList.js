40779200d731a913fdfd8ed553dc87de0105090b
Israa91 <eng.israa.1991@gmail.com>
2020-12-31 21:14:09 +0200

    create the filter functionality Relates #16

--- israa-react-todo/src/components/TodoList.js
+++ israa-react-todo/src/components/TodoList.js
@@ -2,16 +2,38 @@ import React from 'react';
 import {Todo} from './Todo'
 
 
-export const TodoList=({todos,setTodos})=>{
-    todos.map(todo=>{
-        console.log('hi'+todo.text)
-    })
+
+export const TodoList=({todos,todoStatus,setTodos,edit,setEdit,inputValue,setInputValue,updatedValue,setUpdatedValue})=>{
+
+    let filtered =  todos.filter((todo)=>{
+        if(todoStatus==='')
+        return todo
+        else if(todo.completed === true && todoStatus=== "true"){
+            return todo
+        }
+        else if(todo.completed === false && todoStatus=== "false"){
+            return todo
+        }
+       
+       
+     })
+     filtered.map(fff=>console.log(fff.completed))
     
+      console.log({todoStatus})
+     
 return(
     <ul>
         {
-            todos.map((todo)=>(
-                <Todo text={todo.text} key={todo.id} todo={todo} todos={todos}  setTodos={setTodos}/>
+           
+          
+           
+            filtered.map((todo)=>(
+                 console.log(todo),
+                <Todo text={todo.text} key={todo.id} todo={todo} todos={todos}  setTodos={setTodos}
+                setEdit={setEdit}
+                inputValue={inputValue} setInputValue={setInputValue}
+               
+                />
             ))
         }
     </ul>
